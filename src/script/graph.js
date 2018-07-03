1bab819a01e6c27cb792a04775e244efd951da4a
Kazu16182718 <pc16182718@gmail.com>
2018-07-03 17:33:00 +0900

    graphの背景化

--- src/script/graph.js
+++ src/script/graph.js
@@ -1,164 +1,169 @@
 /**
- * グラフを担当するクラス
+ * �O���t��S������N���X
  */
 class Graph extends Chart {
-  /**
-   * コンストラクタ
-   *
-   * グラフを生成して、タイマーを起動する
-   */
-  constructor(id, time) {    
-    var now = new Date();
-    now = now.toLocaleTimeString();
-    now = now.slice(0, 5);
-    
-    var config = {
-      //グラフの種類
-      type: 'line',
-      //データの設定
-      data: {
-        //データ項目のラベル
-        labels: [now],
-        //データセット
-        datasets: [
-          {
-            //凡例
-            label: "+1",
-            //面の表示
-            fill: false,
-            //線のカーブ
-            lineTension: 0,
-            //背景色
-            backgroundColor: "rgba(179,181,198,0.2)",
-            //枠線の色
-            borderColor: "#ff6384",
-            //結合点の枠線の色
-            pointBorderColor: "rgba(179,181,198,1)",
-            //結合点の背景色
-            pointBackgroundColor: "#fff",
-            //結合点のサイズ
-            pointRadius: 5,
-            //結合点のサイズ（ホバーしたとき）
-            pointHoverRadius: 8,
-            //結合点の背景色（ホバーしたとき）
-            pointHoverBackgroundColor: "rgba(179,181,198,1)",
-            //結合点の枠線の色（ホバーしたとき）
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            //結合点より外でマウスホバーを認識する範囲（ピクセル単位）
-            pointHitRadius: 15,
-            //グラフのデータ
-            data: [0]
-        },
-          {
-            //凡例
-            label: "-1",
-            //面の表示
-            fill: false,
-            //線のカーブ
-            lineTension: 0,
-            //背景色
-            backgroundColor: "rgba(75,192,192,0.4)",
-            //枠線の色
-            borderColor: "#36a2eb",
-            //結合点の枠線の色
-            pointBorderColor: "rgba(75,192,192,1)",
-            //結合点の背景色
-            pointBackgroundColor: "#fff",
-            //結合点のサイズ
-            pointRadius: 5,
-            //結合点のサイズ（ホバーしたとき）
-            pointHoverRadius: 8,
-            //結合点の背景色（ホバーしたとき）
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            //結合点の枠線の色（ホバーしたとき）
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            //結合点より外でマウスホバーを認識する範囲（ピクセル単位）
-            pointHitRadius: 10,
-            //グラフのデータ
-            data: []
-        }
-      ]
-      },
-      //オプションの設定
-      options: {
-        //軸の設定
-        scales: {
-          //縦軸の設定
-          yAxes: [{
-            //目盛りの設定
-            ticks: {
-              //最小値を0にする
-              beginAtZero: true
+    /**
+     * �R���X�g���N�^
+     *
+     * �O���t�𐶐����āA�^�C�}�[��N������
+     */
+    constructor(id, time) {
+        var now = new Date();
+        now = now.toLocaleTimeString();
+        now = now.slice(0, 5);
+
+        var config = {
+            //�O���t�̎��
+            type: 'line',
+            //�f�[�^�̐ݒ�
+            data: {
+                //�f�[�^���ڂ̃��x��
+                labels: [now],
+                //�f�[�^�Z�b�g
+                datasets: [
+                    {
+                        //�}��
+                        label: "+1",
+                        //�ʂ̕\��
+                        fill: false,
+                        //���̃J�[�u
+                        lineTension: 0,
+                        //�w�i�F
+                        backgroundColor: "rgba(179,181,198,0.2)",
+                        //�g���̐F
+                        borderColor: "#ff6384",
+                        //�����_�̘g���̐F
+                        pointBorderColor: "rgba(179,181,198,1)",
+                        //�����_�̔w�i�F
+                        pointBackgroundColor: "#fff",
+                        //�����_�̃T�C�Y
+                        pointRadius: 5,
+                        //�����_�̃T�C�Y�i�z�o�[�����Ƃ��j
+                        pointHoverRadius: 8,
+                        //�����_�̔w�i�F�i�z�o�[�����Ƃ��j
+                        pointHoverBackgroundColor: "rgba(179,181,198,1)",
+                        //�����_�̘g���̐F�i�z�o�[�����Ƃ��j
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        //�����_���O�Ń}�E�X�z�o�[��F������͈́i�s�N�Z���P�ʁj
+                        pointHitRadius: 15,
+                        //�O���t�̃f�[�^
+                        data: [0]
+                    },
+                    {
+                        //�}��
+                        label: "-1",
+                        //�ʂ̕\��
+                        fill: false,
+                        //���̃J�[�u
+                        lineTension: 0,
+                        //�w�i�F
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        //�g���̐F
+                        borderColor: "#36a2eb",
+                        //�����_�̘g���̐F
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        //�����_�̔w�i�F
+                        pointBackgroundColor: "#fff",
+                        //�����_�̃T�C�Y
+                        pointRadius: 5,
+                        //�����_�̃T�C�Y�i�z�o�[�����Ƃ��j
+                        pointHoverRadius: 8,
+                        //�����_�̔w�i�F�i�z�o�[�����Ƃ��j
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        //�����_�̘g���̐F�i�z�o�[�����Ƃ��j
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        //�����_���O�Ń}�E�X�z�o�[��F������͈́i�s�N�Z���P�ʁj
+                        pointHitRadius: 10,
+                        //�O���t�̃f�[�^
+                        data: []
+                    }
+                ]
+            },
+            //�I�v�V�����̐ݒ�
+            options: {
+                //���̐ݒ�
+                scales: {
+                    //�c���̐ݒ�
+                    yAxes: [{
+                        //�ڐ���̐ݒ�
+                        ticks: {
+                            //�ŏ��l��0�ɂ���
+                            beginAtZero: true
+                        }
+                    }]
+                },
+                //�z�o�[�̐ݒ�
+                hover: {
+                    //�z�o�[���̓���isingle, label, dataset�j
+                    mode: 'single'
+                }
             }
-        }]
-        },
-        //ホバーの設定
-        hover: {
-          //ホバー時の動作（single, label, dataset）
-          mode: 'single'
         }
-      }
-    }
-    
-    // canvasを読み込む
-    var canvas = document.getElementById(id);
-    super(canvas.getContext('2d'), config);
-    
-    // タイマーを開始
-    this.time = time;
-    this.timer()
-  }
 
-  /**
-   * 現在を表すグラフの値を1加算する
-   */
-  add(lineId) {
-    // 一番新しい要素に1を加算する
-    var index = graph.data.datasets[lineId].data.length - 1;
-    this.data.datasets[lineId].data[index] ++;
-    this.update();
-  }
+        // canvas��ǂݍ���
+        var canvas = document.getElementById(id);
+        super(canvas.getContext('2d'), config);
 
-  /**
-   * グラフのラベルに現在時刻を足す
-   */
-  chop() {
-    // ラベルを足す
-    var time = new Date();
-    time = time.toLocaleTimeString();
-    time = time.slice(0, 5);
-    this.data.labels.push(time);
-    
-    // ゼロ埋めをする
-    var lineId = 0;
-    var index = this.data.labels.length - 1;
-    if (this.data.datasets[lineId].data[index] == undefined) {
-      this.data.datasets[lineId].data[index] = 0;
+        // �^�C�}�[��J�n
+        this.time = time;
+        this.timer()
     }
-    lineId = 1;
-    index = this.data.labels.length - 1;
-    if (this.data.datasets[lineId].data[index] == undefined) {
-      this.data.datasets[lineId].data[index] = 0;
-    }
-    
-    // 画面の更新
-    this.update();
-  }
 
-  /**
-   * 定期的に、chopを実行する
-   */
-  timer() {
-    // 値を計算する
-    this.chop();
-    
-    var self = this;
-    setInterval(
-      () => {
-        self.chop();
-      }, self.time * 1000);
-  }
+
+    /**
+     * ���݂�\���O���t�̒l��1���Z����
+     */
+    add(lineId) {
+        // ��ԐV�����v�f��1����Z����
+        var index = graph.data.datasets[lineId].data.length - 1;
+        this.data.datasets[lineId].data[index]++;
+        this.update();
+    }
+
+
+    /**
+     * �O���t�̃��x���Ɍ��ݎ����𑫂�
+     */
+    chop() {
+        // ���x���𑫂�
+        var time = new Date();
+        time = time.toLocaleTimeString();
+        time = time.slice(0, 5);
+        this.data.labels.push(time);
+
+        // �[�����߂��
+        var lineId = 0;
+        var index = this.data.labels.length - 1;
+        if (this.data.datasets[lineId].data[index] == undefined) {
+            this.data.datasets[lineId].data[index] = 0;
+        }
+        lineId = 1;
+        index = this.data.labels.length - 1;
+        if (this.data.datasets[lineId].data[index] == undefined) {
+            this.data.datasets[lineId].data[index] = 0;
+        }
+
+        // ��ʂ̍X�V
+        this.update();
+    }
+
+
+    /**
+     * ����I�ɁAchop����s����
+     */
+    timer() {
+        // �l��v�Z����
+        this.chop();
+
+        var self = this;
+        setInterval(
+            () => {
+                self.chop();
+            }, self.time * 1000);
+    }
 }
+
+
 
 
 var graph = new Graph("graph", 10);
