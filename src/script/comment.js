46932cd6ee1b239ed885a0e8e84f4ec20e3538b5
norara13 <s16161@tokyo.kosen-ac.jp>
2018-09-12 22:18:08 +0900

    修正
    コメントの挙動がまだおかしい

--- src/script/comment.js
+++ src/script/comment.js
@@ -1,27 +1,30 @@
+var width = window.innerWidth;
+
 class Comment {
     /**
      * コンストラクタ
      *
      * エレメントをラップ
      */
-    constructor(child, minute=300) {
+    constructor(child, minute=10) {
         var self = document.createElement("div");
         self.setAttribute("class", "marquee");
         self.append(child);
         
         ////////////////////////////////////////////
-
         //innerTextの文字数を取得
         var word = self.innerText.length;
         
         //追加ピクセル数
-        var px = word*9.8;
+        var px = word*16;
         
-        var style = "width:" + px + "px";
-        console.log(word);
+        console.log(width);
+        var w = width + px;
+        var style = "width:" + w + "px";
 
         //cssに適応させる
         self.setAttribute( "style", style ) ;
+
        ////////////////////////////////////////////
         
         setTimeout(() => {
@@ -33,7 +36,7 @@ class Comment {
      * 要素自体を消去
      */
     remove() {
-        this.parentNode.removeChild(this);
+       this.parentNode.removeChild(this);
     }
 }
 
@@ -59,3 +62,5 @@ class Screen {
 }
 
 var screen = new Screen("screen");
+screen.send("hoge");
+screen.send("hogehoge");
