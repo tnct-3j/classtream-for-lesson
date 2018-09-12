ae3614fa0e3ce6c7b706967b10624d9d473efe74
norara13 <s16161@tokyo.kosen-ac.jp>
2018-09-12 10:07:58 +0900

    微調節

--- src/script/comment.js
+++ src/script/comment.js
@@ -5,10 +5,25 @@ class Comment {
      * エレメントをラップ
      */
     constructor(child) {
-        self = document.createElement("div");
+        var self = document.createElement("div");
         self.setAttribute("class", "marquee");
         self.append(child);
+        
+        ////////////////////////////////////////////
 
+        //innerTextの文字数を取得
+        var word = self.innerText.length;
+        
+        //追加ピクセル数
+        var px = word*9.8;
+        
+        var style = "width:" + px + "px";
+        console.log(word);
+
+        //cssに適応させる
+        self.setAttribute( "style", style ) ;
+       ////////////////////////////////////////////
+        
         setTimeout(() => {
             self.remove();
         }, 10000);
@@ -40,12 +55,22 @@ class Screen {
         var comment = new Comment(element);
         this.screen.appendChild(comment);
     }
+
 }
 
 var screen = new Screen("screen");
+
+function send(msg) {
+    /* 文字を打ち込むと流れる */
+    var p = document.createElement("p");
+    p.innerText = msg;
+
+    screen.send(p);
+}
 
 // コンストラクタの引数に設定した、
 // element内でhelloって文字が流れる
 var p = document.createElement("p");
 p.innerText = "hello";
+
 screen.send(p);
